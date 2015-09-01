14c9ea6859afff5672889761cf46ac217839de03
Mike Ottum <mike.ottum@gmail.com>
2015-09-01 16:10:47 -0700

    Remove InfluxDB support
    This change removes InfluxDB support, since it's not currently being used by any clients and was breaking tests.
    
    The version is bumped from 0.3.6 to 0.4.0.
    
    This also adds Travis Slack notifications.

--- automation/provision.sh
+++ automation/provision.sh
@@ -1,19 +1,18 @@
-apt-get update
-apt-get install -y nginx git
+set -e
+set -x
 
-apt-get install -y python-software-properties python g++ make
+apt-get -qq update
+apt-get -qq install -y nginx git
+
+apt-get -qq install -y python-software-properties python g++ make
 add-apt-repository ppa:chris-lea/node.js
-apt-get update
-apt-get install -y nodejs
+apt-get -qq update
+apt-get -qq install -y nodejs
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-
-wget http://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
-sudo dpkg -i influxdb_latest_amd64.deb
-sudo /etc/init.d/influxdb start
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+apt-get -qq update
+apt-get -qq install -y mongodb-org
 
 cd /vagrant
 npm install
