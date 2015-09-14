8417efc6d87dddf3f19344f4d1d760308b0b1886
Mike Ottum <mike.ottum@gmail.com>
2015-09-14 18:28:02 -0400

    Vagrant: cd to /vagrant on vagrant ssh

--- automation/provision.sh
+++ automation/provision.sh
@@ -9,3 +9,5 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 apt-get -qq update
 apt-get -qq install -y nodejs mongodb-org
+
+echo "cd /vagrant" >> /home/vagrant/.bashrc
