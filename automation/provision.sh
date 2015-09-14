3c7f2fb49927c0c6af44da218d61581f52a31955
Mike Ottum <mike.ottum@gmail.com>
2015-09-14 14:33:09 -0400

    Vagrant: install node v0.12

--- automation/provision.sh
+++ automation/provision.sh
@@ -2,17 +2,10 @@ set -e
 set -x
 
 apt-get -qq update
-apt-get -qq install -y nginx git
-
-apt-get -qq install -y python-software-properties python g++ make
-add-apt-repository ppa:chris-lea/node.js
-apt-get -qq update
-apt-get -qq install -y nodejs
+apt-get -qq install -y python-software-properties python g++ make curl git
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 apt-get -qq update
-apt-get -qq install -y mongodb-org
-
-cd /vagrant
-npm install
+apt-get -qq install -y nodejs mongodb-org
